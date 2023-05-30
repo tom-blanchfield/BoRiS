4e6a7ecf6b04d34306fa9785500f662f83587fd1
tom-blanchfield <74678154+tom-blanchfield@users.noreply.github.com>
2023-05-30 08:11:24 +0100

    Update app1.py
    Change to 10,000 top raters

--- app1.py
+++ app1.py
@@ -39,7 +39,7 @@ grouped_data = filtered_data.groupby('title')['count'].sum().sort_values(ascendi
 
 # Get top 10,000 raters
 ratings_count = ratings.groupby('user_id').size().reset_index(name='count').sort_values('count', ascending=False)
-top_raters = ratings_count[:5000]['user_id'].tolist()
+top_raters = ratings_count[:10000]['user_id'].tolist()
 
 # Create a DataFrame to store user ratings
 user_ratings = pd.DataFrame(columns=['book_id', 'user_id', 'rating'])
@@ -56,7 +56,7 @@ else:
 
         
     if st.button("Get Recommendations!"):
-        # Get the ratings of the top 5,000 raters
+        # Get the ratings of the top 10,000 raters
         top_raters_ratings = ratings[ratings['user_id'].isin(top_raters)]
         top_raters_ratings = top_raters_ratings.pivot(index='user_id', columns='book_id', values='rating').fillna(0)
 
@@ -68,7 +68,7 @@ else:
         # Replace missing values with median
         user_ratings_pivot = user_ratings_pivot.fillna(user_ratings_pivot.median())
 
-        # Get ratings of top 5,000 raters
+        # Get ratings of top 10,000 raters
         top_raters_ratings = ratings[ratings['user_id'].isin(top_raters)].pivot(index='user_id', columns='book_id', values='rating').fillna(0)
 
         # Merge user's ratings with top raters ratings
