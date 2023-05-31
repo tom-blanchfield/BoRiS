98423239d6878d71fca5a27c2b427b33f869a2f3
tom-blanchfield <74678154+tom-blanchfield@users.noreply.github.com>
2023-05-31 18:59:55 +0100

    Update app1.py

--- app1.py
+++ app1.py
@@ -63,7 +63,7 @@ else:
             image = Image.open(response.raw)
             
             # Adjust the image size
-            st.image(image, caption=title, use_column_width=always)
+            st.image(image, caption=title, use_column_width=True)
         except (requests.HTTPError, OSError) as e:
             st.write(f"Error loading image: {e}")
             
