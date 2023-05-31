3708f1ad75bbd4d63d9d14d9ab3e65deb9b64836
tom-blanchfield <74678154+tom-blanchfield@users.noreply.github.com>
2023-05-31 20:11:08 +0100

    Update app1.py

--- app1.py
+++ app1.py
@@ -33,9 +33,9 @@ selected_authors = st.sidebar.multiselect("Select authors", authors)
 selected_tags = st.sidebar.multiselect("Select genres", tags_to_include)
 
 # Modify the filtered data based on the selected authors
-filtered_data = book_data[book_data['authors'].isin(selected_authors) | book_data['tag_name'].isin(selected_tags)]
+filtered_data = book_data[book_data['authors'].apply(lambda x: x.split(',')[0].strip()).isin(selected_authors) | book_data['tag_name'].isin(selected_tags)]
 
-# Group by book and sort by count on
+# Group by book and sort by count
 grouped_data = filtered_data.groupby('title')['count'].sum().sort_values(ascending=False)
 
 # Get top 1,000 raters
