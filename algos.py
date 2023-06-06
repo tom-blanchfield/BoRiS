ec5983eb85df0e46c4209bc299f34c398c4d35ee
tom-blanchfield <74678154+tom-blanchfield@users.noreply.github.com>
2023-06-06 11:36:03 +0100

    Add files via upload

---
+++ algos.py
@@ -0,0 +1,107 @@
+import pandas as pd
+import numpy as np
+import sklearn as sk
+from sklearn.metrics.pairwise import cosine_similarity, pairwise_distances
+from sklearn.decomposition import NMF
+from scipy.spatial.distance import jaccard, euclidean
+from surprise import SVD, KNNBasic, Dataset, Reader
+from surprise.model_selection import train_test_split, cross_validate
+import streamlit as st
+import altair as alt
+
+# Load the data
+ratings = pd.read_csv('ratings.csv')
+
+# Calculate the number of ratings per user
+user_ratings_count = ratings['user_id'].value_counts()
+
+# Get the top 1,000 users by number of ratings
+top_users = user_ratings_count.nlargest(1000).index
+
+# Filter ratings for the top users
+ratings = ratings[ratings['user_id'].isin(top_users)]
+
+# Filter relevant columns
+ratings = ratings[['user_id', 'book_id', 'rating']]
+
+# Create a pivot table of user ratings
+user_ratings_pivot = ratings.pivot_table(index='user_id', columns='book_id', values='rating', fill_value=0)
+
+# Function to calculate algorithm similarity
+def calculate_similarity(algorithm):
+    similarity_score = None  # Initialize the similarity_score variable
+
+    if algorithm == 'Cosine Similarity':
+        st.text("Calculating Cosine Similarities...")
+        # Get the unique book IDs from train_set
+        train_set_books = user_ratings_pivot.columns
+
+        # Calculate similarity using the user_ratings_pivot pivot table
+        similarities = cosine_similarity(user_ratings_pivot.T)
+        similarity_score = np.mean(similarities)
+        st.text(f"Cosine Similarities calculation complete.\nSimilarity Score: {similarity_score:.4f}")
+        st.text("Cosine Similarity measures the similarity between users based on their \nrating patterns. Higher scores indicate more similar users.")
+
+    elif algorithm == 'Adjusted Cosine Similarity':
+        st.text("Calculating Adjusted Cosine Similarities...")
+        # Calculate mean rating for each book
+        book_means = user_ratings_pivot.mean(axis=0)
+
+        # Subtract mean rating from each user's rating
+        ratings_centered = user_ratings_pivot.sub(book_means, axis=1)
+
+        # Calculate similarity using the centered ratings
+        similarities = cosine_similarity(ratings_centered.T)
+        similarity_score = np.mean(similarities)
+        st.text(f"Adjusted Cosine Similarities calculation complete.\nSimilarity Score: {similarity_score:.4f}")
+        st.text("Adjusted Cosine Similarity measures the similarity between users based \non their rating patterns, while considering the average rating of each book.\nHigher scores indicate more similar users.")
+
+    elif algorithm == 'Jaccard':
+        st.text("Calculating Jaccard Similarities...")
+        # Convert DataFrame to NumPy array
+        user_ratings_array = user_ratings_pivot.values
+
+        # Calculate Jaccard similarity
+        similarities = pairwise_distances(user_ratings_array, metric='jaccard')
+        similarity_score = np.mean(1 - similarities)
+        st.text(f"Jaccard Similarities calculation complete.\nSimilarity Score: {similarity_score:.4f}")
+        st.text("Jaccard Similarity measures the similarity between users based on the \npresence or absence of ratings.\nHigher scores indicate more similar users.")
+
+    elif algorithm == 'Euclidean Distance':
+        st.text("Calculating Euclidean Distances...")
+        # Calculate Euclidean distance
+        similarities = pairwise_distances(user_ratings_pivot, metric='euclidean')
+        similarity_score = np.mean(1 / (1 + similarities))
+        st.text(f"Euclidean Distances calculation complete.\nSimilarity Score: {similarity_score:.4f}")
+        st.text("Euclidean Distance measures the dissimilarity between users based on their \nrating patterns. Lower scores indicate more similar users.")
+
+    elif algorithm == 'Non-Zero Matrix Factorization':
+        st.text("Calculating Non-Zero Matrix Factorization Similarities...")
+        # Implement the NMF algorithm
+        nmf = NMF(n_components=10)
+        nmf.fit(user_ratings_pivot)
+        user_features = nmf.transform(user_ratings_pivot)
+        item_features = nmf.components_
+
+        # Transpose either user_features or item_features
+        # to ensure compatibility for cosine similarity calculation
+        similarities = cosine_similarity(user_features, item_features.T)
+        similarity_score = np.mean(similarities)
+        st.text(f"Non-Zero Matrix Factorization Similarities calculation complete.\nSimilarity Score: {similarity_score:.4f}")
+        st.text("Non-Zero Matrix Factorization calculates the similarity between users based on a \nmatrix factorization approach.\nHigher scores indicate more similar users.")
+
+
+# Streamlit app
+st.title("Recommender Algorithm Evaluator")
+st.sidebar.title("Options")
+
+# Algorithm selection
+algorithm = st.sidebar.selectbox("Select Algorithm", ['Cosine Similarity', 'Adjusted Cosine Similarity', 'Jaccard', 'Euclidean Distance',
+                                                      'Non-Zero Matrix Factorization'])
+
+# Calculate similarity scores
+similarity_scores = {}
+
+# Calculate similarity only if the algorithm is selected
+if algorithm != "":
+    similarity_scores[algorithm] = calculate_similarity(algorithm)
