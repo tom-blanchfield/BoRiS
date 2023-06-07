827ef44d719817c46fd0d722f61074f786862a7e
tom-blanchfield <74678154+tom-blanchfield@users.noreply.github.com>
2023-06-07 23:33:09 +0100

    Update app1.py

--- app1.py
+++ app1.py
@@ -4,6 +4,81 @@ import sklearn as sk
 from sklearn.metrics.pairwise import cosine_similarity
 import requests
 import streamlit as st
+from PIL import Image
+
+# Load the data
+books = pd.read_csv('books.csv')
+book_tags = pd.read_csv('book_tags.csv')
+tags = pd.read_csv('tags.csv')
+ratings = pd.read_csv('ratings.csv')
+
+# Merge tags data
+book_data = pd.merge(books, book_tags, on='goodreads_book_id')
+book_data = pd.merge(book_data, tags, on='tag_id')
+
+# Get top 1000 tags
+tags_df = book_data.groupby('tag_name').size().reset_index(name='counts').sort_values('counts', ascending=False).head(1000)
+
+# Tags to include in the "genres" multi-select dropdown
+tags_to_include = ['literature', 'comedy', 'young-adult', 'romance', 'mystery', 'science-fiction', 'fantasy', 'horror', 'thriller', 'western', 'dystopian', 'memoir', 'biography', 'autobiography', 'history', 'travel', 'cookbook', 'self-help', 'business', 'finance', 'psychology', 'philosophy', 'religion', 'art', 'music', 'comics', 'graphic novels', 'poetry', 'sport', 'humorous', 'war', 'funny']
+
+# Title
+st.sidebar.title("Please choose your favourite authors, and or genres")
+
+# Allow the user to select multiple authors
+authors = list(set(books['authors'].apply(lambda x: x.split(',')[0].strip())))
+selected_authors = st.sidebar.multiselect("Select authors", authors)
+
+#Allow the user to select multiple genres
+selected_tags = st.sidebar.multiselect("Select genres", tags_to_include)
+
+# Modify the filtered data based on the selected authors
+filtered_data = book_data[book_data['authors'].apply(lambda x: x.split(',')[0].strip()).isin(selected_authors) | book_data['tag_name'].isin(selected_tags)]
+
+# Group by book and sort by count
+grouped_data = filtered_data.groupby('title')['count'].sum().sort_values(ascending=False)
+
+# Get top 1,000 raters
+ratings_count = ratings.groupby('user_id').size().reset_index(name='count').sort_values('count', ascending=False)
+top_raters = ratings_count[:1000]['user_id'].tolist()
+
+# Create a DataFrame to store user ratings
+user_ratings = pd.DataFrame(columns=['book_id', 'user_id', 'rating'])
+
+# Display books to rate
+st.title("Please rate these books:")
+if len(grouped_data) == 0:
+    st.write("No books found with selected authors or genres")
+else:
+    for title, count in grouped_data[:40].items():
+        # Get the book ID and image URL
+        book_id = books.loc[books['title'] == title, 'book_id'].values[0]
+        image_url = books.loc[books['title'] == title, 'image_url'].values[0]
+
+        # Download the image from the URL
+        try:
+            response = requests.get(image_url, stream=True)
+            response.raise_for_status()
+            image = Image.open(response.raw)
+            
+            # Adjust the image size
+            st.image(image, use_column_width=False, width=200)
+        except (requests.HTTPError, OSError) as e:
+            st.write(f"Error loading image: {e}")
+            
+        # Ask the user to rate the book
+        rating_input = st.number_input(f"Rate {title} (1-5)", min_value=1, max_value=5, key=title)
+
+        # Store the user's rating in the DataFrame
+        user_ratings = pd.concat([user_ratings, pd.DataFrame({'book_id': [book_id], 'user_id': ['user1'], 'rating': [rating_input]})], ignore_index=True)
+
+        
+ import pandas as pd
+import numpy as np
+import sklearn as sk
+from sklearn.metrics.pairwise import cosine_similarity
+import requests
+import streamlit as st
 import uuid
 from PIL import Image
 
@@ -30,7 +105,7 @@ st.sidebar.title("Please choose your favourite authors, and or genres")
 authors = list(set(books['authors'].apply(lambda x: x.split(',')[0].strip())))
 selected_authors = st.sidebar.multiselect("Select authors", authors)
 
-# Allow the user to select multiple genres
+#Allow the user to select multiple genres
 selected_tags = st.sidebar.multiselect("Select genres", tags_to_include)
 
 # Modify the filtered data based on the selected authors
@@ -51,41 +126,37 @@ st.title("Please rate these books:")
 if len(grouped_data) == 0:
     st.write("No books found with selected authors or genres")
 else:
-    num_books_per_row = 3  # Number of books to display in each row
-    num_books = 40  # Total number of books to display
-    
-    for i in range(0, num_books, num_books_per_row):
-        book_row = grouped_data[i:i+num_books_per_row]  # Get books for the current row
-        
-        columns = st.columns(num_books_per_row)
-        
-        for idx, (title, count) in enumerate(book_row.items()):
-            # Get the book ID and image URL
-            book_id = books.loc[books['title'] == title, 'book_id'].values[0]
-            image_url = books.loc[books['title'] == title, 'image_url'].values[0]
+    # Create three columns for book ratings
+    columns = st.columns(3)
+    count = 0
 
-            # Download the image from the URL
-            try:
-                response = requests.get(image_url, stream=True)
-                response.raise_for_status()
-                image = Image.open(response.raw)
+    for title, count in grouped_data[:40].items():
+        # Get the book ID and image URL
+        book_id = books.loc[books['title'] == title, 'book_id'].values[0]
+        image_url = books.loc[books['title'] == title, 'image_url'].values[0]
 
-                # Adjust the image size and display it with the title
-                image = image.resize((200, 300))
-                columns[idx].image(image, caption=title, use_column_width=False, width=200)
+        # Download the image from the URL
+        try:
+            response = requests.get(image_url, stream=True)
+            response.raise_for_status()
+            image = Image.open(response.raw)
 
-                # Ask the user to rate the book
-                key = f"rating_input_{book_id}_{uuid.uuid4()}"
-                rating_input = columns[idx].number_input(f"Rate {title} (1-5)", min_value=1, max_value=5, key=key)
+            # Adjust the image size
+            image = image.resize((200, 300))
 
-                # Store the user's rating in the DataFrame
-                user_ratings = pd.concat([user_ratings, pd.DataFrame({'book_id': [book_id], 'user_id': ['user1'], 'rating': [rating_input]})], ignore_index=True)
+            # Display the image with the title
+            columns[count % 3].image(image, caption=title, use_column_width=False, width=200)
+        except (requests.HTTPError, OSError) as e:
+            st.write(f"Error loading image: {e}")
 
-            except (requests.HTTPError, OSError) as e:
-                st.write(f"Error loading image: {e}")
+        # Ask the user to rate the book
+        key = f"rating_input_{book_id}_{uuid.uuid4()}"
+        rating_input = columns[count % 3].number_input(f"Rate {title} (1-5)", min_value=1, max_value=5, key=key)
 
-        # Add some spacing between rows
-        st.write("\n")
+        # Store the user's rating in the DataFrame
+        user_ratings = pd.concat([user_ratings, pd.DataFrame({'book_id': [book_id], 'user_id': ['user1'], 'rating': [rating_input]})], ignore_index=True)
+
+        count += 1
         
         if st.button("Get Recommendations!", key=str(uuid.uuid4())):
             # Get the ratings of the top 5,000 raters
@@ -116,7 +187,7 @@ else:
             # Get top rated books of the 10 closest users and sort
             top_rated_books = closest_user_ratings.mean().sort_values(ascending=False)
 
-            # Get recommended books, excluding those containing "Potter"
+            # Get recommended books, excluding those containing Potter
             user_rated_books = user_ratings_df['book_id'].tolist()
             recommended_books = []
             recommended_ids = []
@@ -130,7 +201,7 @@ else:
                         recommended_books.append((title, authors))
                         recommended_ids.append(book_id)
 
-            # Display recommended books
+           # Display recommended books
             if len(recommended_books) == 0:
                 st.write("No book recommendations found.")
             else:
@@ -151,7 +222,6 @@ else:
                         image = Image.open(response.raw)
 
                         # Display the image with the title
-                        st.image(image, caption=title, use_column_width=False, width=200)
-
+                        st.image(image, caption=title, use_column_width=False, width=200, align=center)
                     except (requests.HTTPError, OSError) as e:
                         st.write(f"Error loading image: {e}")
