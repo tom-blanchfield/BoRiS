1ebd7e3a92510fe485032ddef036f120c239ad5b
tom-blanchfield <74678154+tom-blanchfield@users.noreply.github.com>
2023-06-07 23:49:12 +0100

    Update app1.py

--- app1.py
+++ app1.py
@@ -24,16 +24,16 @@ tags_df = book_data.groupby('tag_name').size().reset_index(name='counts').sort_v
 tags_to_include = ['literature', 'comedy', 'young-adult', 'romance', 'mystery', 'science-fiction', 'fantasy', 'horror', 'thriller', 'western', 'dystopian', 'memoir', 'biography', 'autobiography', 'history', 'travel', 'cookbook', 'self-help', 'business', 'finance', 'psychology', 'philosophy', 'religion', 'art', 'music', 'comics', 'graphic novels', 'poetry', 'sport', 'humorous', 'war', 'funny']
 
 # Title
-st.sidebar.title("Please choose your favourite authors, and or genres")
+st.sidebar.title("Please choose your favourite authors and/or genres")
 
 # Allow the user to select multiple authors
 authors = list(set(books['authors'].apply(lambda x: x.split(',')[0].strip())))
 selected_authors = st.sidebar.multiselect("Select authors", authors)
 
-#Allow the user to select multiple genres
+# Allow the user to select multiple genres
 selected_tags = st.sidebar.multiselect("Select genres", tags_to_include)
 
-# Modify the filtered data based on the selected authors
+# Modify the filtered data based on the selected authors and genres
 filtered_data = book_data[book_data['authors'].apply(lambda x: x.split(',')[0].strip()).isin(selected_authors) | book_data['tag_name'].isin(selected_tags)]
 
 # Group by book and sort by count
@@ -82,71 +82,63 @@ else:
         user_ratings = pd.concat([user_ratings, pd.DataFrame({'book_id': [book_id], 'user_id': ['user1'], 'rating': [rating_input]})], ignore_index=True)
 
         count += 1
-        
+
     if st.button("Get Recommendations!", key=str(uuid.uuid4())):
-            # Get the ratings of the top 5,000 raters
-            top_raters_ratings = ratings[ratings['user_id'].isin(top_raters)]
-            top_raters_ratings = top_raters_ratings.pivot(index='user_id', columns='book_id', values='rating').fillna(0)
+        # Get the ratings of the top 5,000 raters
+        top_raters_ratings = ratings[ratings['user_id'].isin(top_raters)]
+        top_raters_ratings = top_raters_ratings.pivot(index='user_id', columns='book_id', values='rating').fillna(0)
 
-            # Add the user's ratings to the DataFrame
-            user_ratings_df = pd.DataFrame(user_ratings)
-            user_ratings_pivot = user_ratings_df.pivot(index='user_id', columns='book_id', values='rating').fillna(0)
-            user_ratings_pivot = user_ratings_pivot.reindex(columns=top_raters_ratings.columns, fill_value=0)
+        # Add the user's ratings to the DataFrame
+        user_ratings_df = pd.DataFrame(user_ratings)
+        user_ratings_pivot = user_ratings_df.pivot(index='user_id', columns='book_id', values='rating').fillna(0)
+        user_ratings_pivot = user_ratings_pivot.reindex(columns=top_raters_ratings.columns, fill_value=0)
 
-            # Replace missing values with median
-            user_ratings_pivot = user_ratings_pivot.fillna(user_ratings_pivot.median())
+        # Replace missing values with median
+        user_ratings_pivot = user_ratings_pivot.fillna(user_ratings_pivot.median())
 
-            # Get ratings of top 1,000 raters
-            top_raters_ratings = ratings[ratings['user_id'].isin(top_raters)].pivot(index='user_id', columns='book_id', values='rating').fillna(0)
+        # Get ratings of top 1,000 raters
+        top_raters_ratings = ratings[ratings['user_id'].isin(top_raters)].pivot(index='user_id', columns='book_id', values='rating').fillna(0)
 
-            # Merge user's ratings with top raters ratings
-            merged_ratings = pd.concat([user_ratings_pivot, top_raters_ratings])
+        # Merge user's ratings with top raters ratings
+        merged_ratings = pd.concat([user_ratings_pivot, top_raters_ratings])
 
-            # Calculate cosine similarities between the user and top raters
-            user_similarities = cosine_similarity(merged_ratings)[0]
+        # Calculate cosine similarities between the user and top raters
+        user_similarities = cosine_similarity(merged_ratings)[0]
 
-            # Get the indices of the 10 closest users and their ratings
-            closest_user_indices = user_similarities.argsort()[-11:-1]
-            closest_user_ratings = merged_ratings.iloc[closest_user_indices]
+        # Get the indices of the 10 closest users and their ratings
+        closest_user_indices = user_similarities.argsort()[-11:-1]
+        closest_user_ratings = merged_ratings.iloc[closest_user_indices]
 
-            # Get top rated books of the 10 closest users and sort
-            top_rated_books = closest_user_ratings.mean().sort_values(ascending=False)
+        # Get top rated books of the 10 closest users and sort
+        top_rated_books = closest_user_ratings.mean().sort_values(ascending=False)
 
-            # Get recommended books, excluding those containing Potter
-            user_rated_books = user_ratings_df['book_id'].tolist()
-            recommended_books = []
-            recommended_ids = []
-            for book_id in top_rated_books.index:
-                if len(recommended_books) >= 40:
-                    break
-                title = books.loc[books['book_id'] == book_id, 'title'].values[0]
-                authors = books.loc[books['book_id'] == book_id, 'authors'].values[0]
-                if 'Potter' not in title and book_id not in user_rated_books:
-                    if title not in recommended_books:
-                        recommended_books.append((title, authors))
-                        recommended_ids.append(book_id)
+        # Get recommended books, excluding those containing 'Potter'
+        user_rated_books = user_ratings_df['book_id'].tolist()
+        recommended_books = []
+        recommended_ids = []
+        for book_id in top_rated_books.index:
+            if len(recommended_books) >= 40:
+                break
+            title = books.loc[books['book_id'] == book_id, 'title'].values[0]
+            authors = books.loc[books['book_id'] == book_id, 'authors'].values[0]
+            if 'Potter' not in title and book_id not in user_rated_books:
+                if title not in recommended_books:
+                    recommended_books.append((title, authors))
+                    recommended_ids.append(book_id)
 
-           # Display recommended books
-            if len(recommended_books) == 0:
-                st.write("No book recommendations found.")
-            else:
-                st.write("Recommended books:")
-                for book in recommended_books:
-                    title = book[0]
-                    authors = book[1]
-                    st.write("- {} by {}".format(title, authors))
+        # Display recommended books
+        if len(recommended_books) == 0:
+            st.write("No book recommendations found.")
+        else:
+            st.write("Recommended books:")
+            columns = st.columns(3)  # Create three columns for book recommendations
+            count = 0
 
-                    # Get the book ID and image URL
-                    book_id = books.loc[books['title'] == title, 'book_id'].values[0]
-                    image_url = books.loc[books['title'] == title, 'image_url'].values[0]
+            for book in recommended_books:
+                title = book[0]
+                authors = book[1]
+                st.write("- {} by {}".format(title, authors))
 
-                    # Download the image from the URL
-                    try:
-                        response = requests.get(image_url, stream=True)
-                        response.raise_for_status()
-                        image = Image.open(response.raw)
+                # Get the book ID and image URL
+                book_id = books.loc[books['title']
 
-                        # Display the image with the title
-                        st.image(image, caption=title, use_column_width=False, width=200, align=center)
-                    except (requests.HTTPError, OSError) as e:
-                        st.write(f"Error loading image: {e}")
