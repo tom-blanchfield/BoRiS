5afbf2ee47cda54d9794aa5aa16ca7c026693c30
tom-blanchfield <74678154+tom-blanchfield@users.noreply.github.com>
2023-06-08 21:22:12 +0100

    Update app1.py

--- app1.py
+++ app1.py
@@ -37,9 +37,9 @@ filtered_data = book_data[book_data['authors'].isin(selected_authors) | book_dat
 # Group by book and sort by count
 grouped_data = filtered_data.groupby('title')['count'].sum().sort_values(ascending=False)
 
-# Get top 10,000 raters
+# Get top 5,000 raters
 ratings_count = ratings.groupby('user_id').size().reset_index(name='count').sort_values('count', ascending=False)
-top_raters = ratings_count[:1000]['user_id'].tolist()
+top_raters = ratings_count[:5000]['user_id'].tolist()
 
 # Create a DataFrame to store user ratings
 user_ratings = pd.DataFrame(columns=['book_id', 'user_id', 'rating'])
@@ -55,7 +55,7 @@ else:
         user_ratings = user_ratings.append({'book_id': book_id, 'user_id': 'user1', 'rating': rating_input}, ignore_index=True)
 
     if st.button("Get Recommendations!"):
-        # Get the ratings of the top 10,000 raters
+        # Get the ratings of the top 5,000 raters
         top_raters_ratings = ratings[ratings['user_id'].isin(top_raters)]
         top_raters_ratings = top_raters_ratings.pivot(index='user_id', columns='book_id', values='rating').fillna(0)
 
@@ -69,40 +69,6 @@ else:
 
         # Calculate similarity scores using cosine similarity
         user_similarities = pw.cosine_similarity(user_ratings_pivot, dense_output=False)[0]
-
-        # Get the indices of the 10 closest users and their ratings
-        closest_user_indices = user_similarities.argsort()[-11:-1]
-        # Display books to rate
-st.title("Please rate these books:")
-if len(grouped_data) == 0:
-    st.write("No books found with selected authors or genres")
-else:
-    for title, count in grouped_data[:100].items():
-        rating_input = st.number_input(f"Rate {title} (1-5)", min_value=1, max_value=5, key=title)
-        book_id = books.loc[books['title'] == title, 'book_id'].values[0]
-        user_ratings = pd.concat([user_ratings, pd.DataFrame({'book_id': [book_id], 'user_id': ['user1'], 'rating': [rating_input]})])
-
-    if st.button("Get Recommendations!"):
-        # Get the ratings of the top 5,000 raters
-        top_raters_ratings = ratings[ratings['user_id'].isin(top_raters)]
-        top_raters_ratings = top_raters_ratings.pivot(index='user_id', columns='book_id', values='rating').fillna(0)
-
-        # Add the user's ratings to the DataFrame
-        user_ratings_df = pd.DataFrame(user_ratings)
-        user_ratings_pivot = user_ratings_df.pivot(index='user_id', columns='book_id', values='rating').fillna(0)
-        user_ratings_pivot = user_ratings_pivot.reindex(columns=top_raters_ratings.columns, fill_value=0)
-
-        # Replace missing values with median
-        user_ratings_pivot = user_ratings_pivot.fillna(user_ratings_pivot.median())
-
-        # Get ratings of top 5,000 raters
-        top_raters_ratings = ratings[ratings['user_id'].isin(top_raters)].pivot(index='user_id', columns='book_id', values='rating').fillna(0)
-
-        # Merge user's ratings with top raters ratings
-        merged_ratings = pd.concat([user_ratings_pivot, top_raters_ratings])
-
-        # Calculate similarity scores using adjusted cosine similarity
-        user_similarities = pw.cosine_similarity(merged_ratings, dense_output=False)[0]
 
         # Get the indices of the 10 closest users and their ratings
         closest_user_indices = user_similarities.argsort()[-11:-1]
