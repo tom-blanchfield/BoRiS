4e3a6c165362c09f9c7f8fd3cbb7d02075650a1d
tom-blanchfield <74678154+tom-blanchfield@users.noreply.github.com>
2023-06-08 18:57:07 +0100

    Update app1.py

--- app1.py
+++ app1.py
@@ -17,7 +17,7 @@ book_data = pd.merge(book_data, tags, on='tag_id')
 tags_df = book_data.groupby('tag_name').size().reset_index(name='counts').sort_values('counts', ascending=False).head(1000)
 
 # Tags to include in the "genres" multi-select dropdown
-tags_to_include = ['young-adult', 'literature', 'romance', 'mystery', 'science-fiction', 'fantasy', 'horror', 'thriller', 'western', 'dystopian', 'memoir', 'biography', 'autobiography', 'history', 'travel', 'cookbook', 'self-help', 'business', 'finance', 'psychology', 'philosophy', 'religion', 'art', 'music', 'comics', 'graphic novels', 'poetry', 'sport', 'humorous', 'war', 'funny']
+tags_to_include = ['comedy', 'literature', 'young-adult', 'romance', 'mystery', 'science-fiction', 'fantasy', 'horror', 'thriller', 'western', 'dystopian', 'memoir', 'biography', 'autobiography', 'history', 'travel', 'cookbook', 'self-help', 'business', 'finance', 'psychology', 'philosophy', 'religion', 'art', 'music', 'comics', 'graphic novels', 'poetry', 'sport', 'humorous', 'war', 'funny']
 
 # Extract unique authors
 unique_authors = books['authors'].unique()
@@ -26,7 +26,9 @@ unique_authors = books['authors'].unique()
 st.sidebar.title("Please choose your favourite authors and/or genres")
 
 # Allow the user to select multiple authors
-selected_authors = st.sidebar.multiselect("Select authors", unique_authors)
+selected_authors = st.sidebar.multiselect("Select authors", list(set(books['authors'].apply(lambda x: x.split(',')[0].strip()))))
+
+
 
 # Allow the user to select multiple genres
 selected_tags = st.sidebar.multiselect("Select genres", tags_to_include)
@@ -37,9 +39,9 @@ filtered_data = book_data[book_data['authors'].isin(selected_authors) | book_dat
 # Group by book and sort by count
 grouped_data = filtered_data.groupby('title')['count'].sum().sort_values(ascending=False)
 
-# Get top 10,000 raters
+# Get top 5,000 raters
 ratings_count = ratings.groupby('user_id').size().reset_index(name='count').sort_values('count', ascending=False)
-top_raters = ratings_count[:1000]['user_id'].tolist()
+top_raters = ratings_count[:5000]['user_id'].tolist()
 
 # Create a DataFrame to store user ratings
 user_ratings = pd.DataFrame(columns=['book_id', 'user_id', 'rating'])
@@ -55,7 +57,7 @@ else:
         user_ratings = user_ratings.append({'book_id': book_id, 'user_id': 'user1', 'rating': rating_input}, ignore_index=True)
 
     if st.button("Get Recommendations!"):
-        # Get the ratings of the top 10,000 raters
+        # Get the ratings of the top 5,000 raters
         top_raters_ratings = ratings[ratings['user_id'].isin(top_raters)]
         top_raters_ratings = top_raters_ratings.pivot(index='user_id', columns='book_id', values='rating').fillna(0)
 
@@ -104,5 +106,4 @@ else:
             st.write("Recommended books (Adjusted cosine similarity):")
             for book in recommended_books:
                 st.write("- {} by {}".format(book[0], book[1]))
-
 
