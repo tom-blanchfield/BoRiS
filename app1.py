d27b9f2a1beff4ebe9ee168ea8dfc408a3fd19bc
tom-blanchfield <74678154+tom-blanchfield@users.noreply.github.com>
2023-06-11 15:20:35 +0100

    Update app1.py

--- app1.py
+++ app1.py
@@ -20,7 +20,7 @@ book_data = pd.merge(book_data, tags, on='tag_id')
 tags_df = book_data.groupby('tag_name').size().reset_index(name='counts').sort_values('counts', ascending=False).head(1000)
 
 # Tags to include in the "genres" multi-select dropdown
-tags_to_include = ['literature', 'comedy', 'young-adult', 'romance', 'mystery', 'science-fiction', 'fantasy', 'horror', 'thriller', 'western', 'dystopian', 'memoir', 'biography', 'autobiography', 'history', 'travel', 'cookbook', 'self-help', 'business', 'finance', 'psychology', 'philosophy', 'religion', 'art', 'music', 'comics', 'graphic novels', 'poetry', 'sport', 'humorous', 'war', 'funny']
+tags_to_include = ['literature', 'comedy', 'young-adult', 'romance', 'mystery', 'suicide', 'science-fiction', 'fantasy', 'horror', 'thriller', 'western', 'dystopian', 'memoir', 'biography', 'autobiography', 'history', 'travel', 'cookbook', 'self-help', 'business', 'finance', 'psychology', 'philosophy', 'religion', 'art', 'music', 'comics', 'graphic novels', 'poetry', 'sport', 'humorous', 'war', 'funny']
 
 # Title
 st.sidebar.title("Please choose your favourite authors and/or genres")
@@ -51,7 +51,7 @@ if len(grouped_data) == 0:
     st.write("No books found with selected authors or genres")
 else:
     columns = st.columns(3)
-    for column_idx, (title, count) in enumerate(grouped_data[:40].items()):
+    for column_idx, (title, count) in enumerate(grouped_data[:100].items()):
         # Get the book ID and image URL
         book_id = books.loc[books['title'] == title, 'book_id'].values[0]
         image_url = books.loc[books['title'] == title, 'image_url'].values[0]
