549c86502c2995b9f33cc3d366e28421df0f721c
tom-blanchfield <74678154+tom-blanchfield@users.noreply.github.com>
2023-06-13 14:46:09 +0100

    Update app1.py

--- app1.py
+++ app1.py
@@ -96,7 +96,8 @@ def export_csv(data):
 
 # Get recommendations if button is clicked
 if st.button("Get Recommendations!"):
-    if (selection_type == "Authors" and len(selected_authors) > 0) or selection_type == "Genres" and len(selected_genres) > 0):
+    if (selection_type == "Authors" and len(selected_authors) > 0) or (selection_type == "Genres" and len(selected_genres) > 0):
+
         
         # Get the ratings of the top 2,000 raters
         top_raters = ratings.groupby('user_id').size().nlargest(2000).index.tolist()
