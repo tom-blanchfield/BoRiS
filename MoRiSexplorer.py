722be81a4c81ef3f4487b856511be3ab5ae2d48e
tom-blanchfield <74678154+tom-blanchfield@users.noreply.github.com>
2023-07-13 01:38:35 +0100

    Update MoRiSexplorer.py

--- MoRiSexplorer.py
+++ MoRiSexplorer.py
@@ -1,109 +1,199 @@
 import pandas as pd
-import numpy as np
-import sklearn.metrics.pairwise as pw
+import altair as alt
 import streamlit as st
-
-# Load the data
-books = pd.read_csv('books.csv')
-book_tags = pd.read_csv('book_tags.csv')
-tags = pd.read_csv('tags.csv')
-ratings = pd.read_csv('ratings.csv')
-
-# Merge tags data
-book_data = pd.merge(books, book_tags, on='goodreads_book_id')
-book_data = pd.merge(book_data, tags, on='tag_id')
-
-# Get top 1000 tags
-tags_df = book_data.groupby('tag_name').size().reset_index(name='counts').sort_values('counts', ascending=False).head(1000)
-
-# Tags to include in the "genres" multi-select dropdown
-tags_to_include = ['young-adult', 'literature', 'romance', 'mystery', 'science-fiction', 'fantasy', 'horror', 'thriller', 'western', 'dystopian', 'memoir', 'biography', 'autobiography', 'history', 'travel', 'cookbook', 'self-help', 'business', 'finance', 'psychology', 'philosophy', 'religion', 'art', 'music', 'comics', 'graphic novels', 'poetry', 'sport', 'humorous', 'war', 'funny']
-
-# Extract unique authors
-unique_authors = books['authors'].unique()
-
-# Title
-st.sidebar.title("Please choose your favourite authors and/or genres")
-
-# Allow the user to select multiple authors
-selected_authors = st.sidebar.multiselect("Select authors", unique_authors)
-
-# Allow the user to select multiple genres
-selected_tags = st.sidebar.multiselect("Select genres", tags_to_include)
-
-# Modify the filtered data based on the selected authors
-filtered_data = book_data[book_data['authors'].isin(selected_authors) | book_data['tag_name'].isin(selected_tags)]
-
-# Group by book and sort by count
-grouped_data = filtered_data.groupby('title')['count'].sum().sort_values(ascending=False)
-
-# Get top 1,000 raters
-ratings_count = ratings.groupby('user_id').size().reset_index(name='count').sort_values('count', ascending=False)
-top_raters = ratings_count[:1000]['user_id'].tolist()
-
-# Create a DataFrame to store user ratings
-user_ratings = pd.DataFrame(columns=['book_id', 'user_id', 'rating'])
-
-# Display books to rate
-st.title("Please rate these books:")
-if len(grouped_data) == 0:
-    st.write("No books found with selected authors or genres")
-else:
-    for title, count in grouped_data[:100].items():
-        rating_input = st.number_input(f"Rate {title} (1-5)", min_value=1, max_value=5, key=title)
-        book_id = books.loc[books['title'] == title, 'book_id'].values[0]
-        user_ratings = user_ratings.append({'book_id': book_id, 'user_id': 'user1', 'rating': rating_input}, ignore_index=True)
-
-    if st.button("Get Recommendations!"):
-        # Get the ratings of the top 10,000 raters
-        top_raters_ratings = ratings[ratings['user_id'].isin(top_raters)]
-        top_raters_ratings = top_raters_ratings.pivot(index='user_id', columns='book_id', values='rating').fillna(0)
-
-        # Add the user's ratings to the DataFrame
-        user_ratings_df = pd.DataFrame(user_ratings)
-        user_ratings_pivot = user_ratings_df.pivot(index='user_id', columns='book_id', values='rating').fillna(0)
-        user_ratings_pivot = user_ratings_pivot.reindex(columns=top_raters_ratings.columns, fill_value=0)
-
-        # Replace missing values with median
-        user_ratings_pivot = user_ratings_pivot.fillna(user_ratings_pivot.median())
-
-        # Get ratings of top 1,000 raters
-        top_raters_ratings = ratings[ratings['user_id'].isin(top_raters)].pivot(index='user_id', columns='book_id', values='rating').fillna(0)
-
-        # Merge user's ratings with top raters ratings
-        merged_ratings = pd.concat([user_ratings_pivot, top_raters_ratings])
-
-        # Calculate similarity scores using adjusted cosine similarity
-        user_similarities = pw.cosine_similarity(merged_ratings, dense_output=False)[0]
-
-        # Get the indices of the 10 closest users and their ratings
-        closest_user_indices = user_similarities.argsort()[-11:-1]
-        closest_user_ratings = merged_ratings.iloc[closest_user_indices]
-
-        # Get top rated books of the 10 closest users and sort
-        top_rated_books = closest_user_ratings.mean().sort_values(ascending=False)
-        
-        # Get recommended books, excluding those containing Potter
-        user_rated_books = user_ratings_df['book_id'].tolist()
-        recommended_books = []
-        recommended_ids = []
-
-        for book_id in top_rated_books.index:
-            if len(recommended_books) >= 100:
-                break
-            title = books.loc[books['book_id'] == book_id, 'title'].values[0]
-            authors = books.loc[books['book_id'] == book_id, 'authors'].values[0]
-            if 'Potter' not in title and book_id not in user_rated_books:
-                if title not in recommended_books:
-                    recommended_books.append((title, authors))
-                    recommended_ids.append(book_id)
-
-        if len(recommended_books) == 0:
-            st.write("No book recommendations found for adjusted cosine similarity.")
-        else:
-            st.write("Recommended books:")
-            for book in recommended_books:
-                st.write("- {} by {}".format(book[0], book[1]))
+import numpy as np
+from surprise import Dataset, Reader, KNNWithMeans, SVD, NMF
+from surprise.model_selection import cross_validate
+from surprise import accuracy
+from sklearn.metrics.pairwise import cosine_similarity
+from scipy import sparse
+import matplotlib.pyplot as plt
 
 
+# Load the small dataset
+movies_metadata = pd.read_csv('movies_metadata.csv')
+ratings = pd.read_csv('ratings_small.csv')
+
+# Select the top 20% of raters based on the number of ratings
+top_raters = ratings['userId'].value_counts().head(int(len(ratings['userId'].unique()) * 0.2)).index
+filtered_ratings = ratings[ratings['userId'].isin(top_raters)]
+
+# Data exploration app
+def data_explorer():
+    st.title("Movie Data Explorer")
+
+    # Show some basic statistics
+    st.write("Number of movies:", movies_metadata.shape[0])
+
+    # Distribution of Average Ratings
+    st.header("Distribution of Average Ratings")
+    average_ratings = movies_metadata['vote_average']
+    average_ratings_filtered = average_ratings[average_ratings.notnull()]
+    average_ratings_df = pd.DataFrame({
+        'Average Rating': average_ratings_filtered
+    })
+    fig = alt.Chart(average_ratings_df).mark_bar().encode(
+        alt.X('Average Rating', bin=alt.Bin(maxbins=50)),
+        y='count()'
+    )
+    st.altair_chart(fig, use_container_width=True)
+
+    # Scatterplot of the Number of Ratings per User
+    st.header("Number of Ratings per User")
+    user_ratings_counts = filtered_ratings['userId'].value_counts()
+    scatterplot = alt.Chart(user_ratings_counts.reset_index()).mark_circle().encode(
+        x='index',
+        y='userId',
+        size='userId',
+        tooltip=['index', 'userId']
+    ).properties(
+        width=600,
+        height=400
+    )
+    st.altair_chart(scatterplot, use_container_width=True)
+
+    # Top 20 Genres
+    st.header("Top 50 Genres")
+    genres = movies_metadata['genres'].str.split(",").explode().str.strip()
+    genre_counts = genres.value_counts().head(50)
+    genre_counts_df = pd.DataFrame({
+        'Genre': genre_counts.index,
+        'Count': genre_counts.values
+    })
+    bar_chart = alt.Chart(genre_counts_df).mark_bar().encode(
+        x='Genre',
+        y='Count'
+    )
+    st.altair_chart(bar_chart, use_container_width=True)
+
+
+    # Movie Release Year Distribution
+    st.header("Movie Release Year Distribution")
+    release_years = pd.to_datetime(movies_metadata['release_date'], errors='coerce').dt.year
+    release_years_filtered = release_years[release_years.notnull()]
+    release_years_df = pd.DataFrame({
+        'Release Year': release_years_filtered
+    })
+    hist = alt.Chart(release_years_df).mark_bar().encode(
+        alt.X('Release Year', bin=alt.Bin(maxbins=50)),
+        y='count()'
+    )
+    st.altair_chart(hist, use_container_width=True)
+
+
+# Item-based Collaborative Filtering
+#def item_based_collaborative_filtering(dataset):
+#    # Compute item-item similarity matrix
+#    similarity_matrix = compute_item_similarity(dataset)
+#
+#    # Make predictions
+#    test_set = dataset.build_testset()
+#    predictions = []
+#    for uid, iid, true_r in test_set:
+#        # Get similar items to the current item
+#        similar_items = similarity_matrix[iid]
+#        # Compute weighted average of ratings of similar items
+#        weighted_sum = np.dot(similar_items, train_set.ur[test_set.to_inner_iid(iid)])
+#        # Normalize the weighted sum by the sum of similarities
+#        prediction = weighted_sum / np.sum(similar_items)
+#        # Clip the prediction within the rating range
+#        prediction = np.clip(prediction, train_set.rating_scale[0], train_set.rating_scale[1])
+#        predictions.append((uid, iid, prediction, true_r))
+#
+#    # Compute RMSE
+#    pred_df = pd.DataFrame(predictions, columns=['uid', 'iid', 'est', 'true_r'])
+#    rmse = accuracy.rmse(pred_df)
+#
+#    return pred_df
+
+# Singular Value Decomposition (SVD)
+def singular_value_decomposition(dataset):
+    algo = SVD()
+    results = cross_validate(algo, dataset, measures=['RMSE'], cv=5, verbose=False)
+    return results
+
+# Alternating Least Squares (ALS)
+def alternating_least_squares(dataset):
+    algo = NMF()
+    results = cross_validate(algo, dataset, measures=['RMSE'], cv=5, verbose=False)
+    return results
+
+# Algorithm evaluator app
+def algorithm_evaluator():
+    st.title("Algorithm Evaluator")
+
+    # Select the top 20% of raters based on the number of ratings
+    top_raters = filtered_ratings['userId'].value_counts().head(int(len(filtered_ratings['userId'].unique()) * 0.2)).index
+    filtered_ratings_top20 = filtered_ratings[filtered_ratings['userId'].isin(top_raters)]
+
+    # Create the Surprise dataset with the filtered ratings
+    surprise_dataset = Dataset.load_from_df(filtered_ratings_top20[['userId', 'movieId', 'rating']], Reader())
+
+    # Build the train set
+    train_set = surprise_dataset.build_full_trainset()
+
+    # Perform KNNWithMeans
+    st.header("KNNWithMeans")
+    knn = KNNWithMeans()
+    knn.fit(train_set)
+    knn_predictions = knn.test(train_set.build_testset())
+    knn_rmse = accuracy.rmse(knn_predictions)
+    st.write("KNNWithMeans RMSE:", knn_rmse)
+
+    # Perform NMF
+    st.header("Non-negative Matrix Factorization (NMF)")
+    nmf = NMF()
+    nmf.fit(train_set)
+    nmf_predictions = nmf.test(train_set.build_testset())
+    nmf_rmse = accuracy.rmse(nmf_predictions)
+    st.write("NMF RMSE:", nmf_rmse)
+
+    # Perform item-based collaborative filtering
+    #st.header("Item-based Collaborative Filtering (IBCF)")
+    #item_based_results = item_based_collaborative_filtering(surprise_dataset)
+    #st.write("Item-based Collaborative Filtering Results:")
+    #st.write("RMSE:", np.mean(item_based_results['test_rmse']))
+
+    # Perform Singular Value Decomposition (SVD)
+    st.header("Singular Value Decomposition (SVD)")
+    svd_results = singular_value_decomposition(surprise_dataset)
+    st.write("SVD Results:")
+    st.write("RMSE:", np.mean(svd_results['test_rmse']))
+
+    # Perform Alternating Least Squares (ALS)
+    st.header("Alternating Least Squares (ALS)")
+    als_results = alternating_least_squares(surprise_dataset)
+    st.write("ALS Results:")
+    st.write("RMSE:", np.mean(als_results['test_rmse']))
+
+    # Visualize algorithm performance
+    algorithms = ['KNNWithMeans','Non-negative Matrix Factorization (NMF)', 'Singular Value Decomposition (SVD)', 'Alternating Least Squares (ALS)']
+    rmse_values = []
+
+    # Append the RMSE values from each algorithm's results
+    rmse_values.append(knn_rmse)
+    rmse_values.append(nmf_rmse)
+    #rmse_values.append(np.mean(item_based_results['test_rmse']))
+    rmse_values.append(np.mean(svd_results['test_rmse']))
+    rmse_values.append(np.mean(als_results['test_rmse']))
+
+    plt.bar(algorithms, rmse_values)
+    plt.xlabel('Algorithm')
+    plt.ylabel('RMSE')
+    plt.title('Algorithm Performance')
+    plt.xticks(rotation=45)  # Rotate x-axis labels for better readability
+    st.pyplot(plt)
+
+# Main app
+def main():
+    st.sidebar.title("Navigation")
+    app_mode = st.sidebar.selectbox("Choose App Mode", ["Data Explorer", "Algorithm Evaluator"])
+
+    if app_mode == "Data Explorer":
+        data_explorer()
+    elif app_mode == "Algorithm Evaluator":
+        algorithm_evaluator()
+
+if __name__ == "__main__":
+    main()
 
