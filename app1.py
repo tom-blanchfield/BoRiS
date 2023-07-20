d07e64e5f42efbd629cdc12def64ff25205113f0
tom-blanchfield <74678154+tom-blanchfield@users.noreply.github.com>
2023-07-20 05:43:00 +0100

    Update app1.py

--- app1.py
+++ app1.py
@@ -198,55 +198,55 @@ def get_author_recommendations(selected_authors, selected_authors_exclude):
             csv_data = [(title, author) for title, author in recommended_books]
             csv_file = export_csv(csv_data)
             st.markdown(f"### [Download Recommended Books CSV](data:file/csv;base64,{base64.b64encode(open(csv_file, 'rb').read()).decode()})")
+
 # Get recommendations if button is clicked
-#if st.button("Get Recommendations!"):
-  if (selection_type == "Authors" and len(selected_authors) > 0) or (selection_type == "Genres" and len(selected_genres) > 0):
-      if selection_type == "Authors":
-          get_author_recommendations(selected_authors, selected_authors_exclude)
-      else:
-          # Filter books by selected genres
-          filtered_data = book_data[book_data['tag_name'].isin(selected_genres)]
+if (selection_type == "Authors" and len(selected_authors) > 0) or (selection_type == "Genres" and len(selected_genres) > 0):
+    if selection_type == "Authors":
+        get_author_recommendations(selected_authors, selected_authors_exclude)
+    else:
+        # Filter books by selected genres
+        filtered_data = book_data[book_data['tag_name'].isin(selected_genres)]
 
-          # Group by book and sort by count
-          grouped_data = filtered_data.groupby('tag_name').apply(lambda x: x.nlargest(21, 'count')).reset_index(drop=True)
+        # Group by book and sort by count
+        grouped_data = filtered_data.groupby('tag_name').apply(lambda x: x.nlargest(21, 'count')).reset_index(drop=True)
 
-          st.write("Your genre-based recommendations:")
+        st.write("Your genre-based recommendations:")
 
-          columns = st.columns(3)
-          included_books = set()
+        columns = st.columns(3)
+        included_books = set()
 
-            for column_idx, book in grouped_data.iterrows():
-                title = book['title']
-                author = book['authors'].split(',')[0].strip()
-                book_id = book['book_id']
-                image_url = book['image_url']
+        for column_idx, book in grouped_data.iterrows():
+            title = book['title']
+            author = book['authors'].split(',')[0].strip()
+            book_id = book['book_id']
+            image_url = book['image_url']
 
-                # Skip if the book is already displayed or in the exclude list
-                if title in included_books or author in selected_authors_exclude:
-                    continue
+            # Skip if the book is already displayed or in the exclude list
+            if title in included_books or author in selected_authors_exclude:
+                continue
 
-                # Download the image from the URL
-                try:
-                    response = requests.get(image_url, stream=True)
-                    response.raise_for_status()
-                    image = Image.open(response.raw)
+            # Download the image from the URL
+            try:
+                response = requests.get(image_url, stream=True)
+                response.raise_for_status()
+                image = Image.open(response.raw)
 
-                    # Adjust the image size
-                    resized_image = image.resize((200, 300))
+                # Adjust the image size
+                resized_image = image.resize((200, 300))
 
-                    # Display the book cover image, title, and author
-                    with columns[column_idx % 3]:
-                        st.image(resized_image, caption=f"{title} by {author}", use_column_width=True)
+                # Display the book cover image, title, and author
+                with columns[column_idx % 3]:
+                    st.image(resized_image, caption=f"{title} by {author}", use_column_width=True)
 
-                    # Add rating of 5 to user's ratings
-                    #user_ratings = user_ratings.append({'book_id': book_id, 'user_id': 'user_id', 'rating': 5}, ignore_index=True)
+                # Add rating of 5 to user's ratings
+                #user_ratings = user_ratings.append({'book_id': book_id, 'user_id': 'user_id', 'rating': 5}, ignore_index=True)
 
-                    included_books.add(title)
+                included_books.add(title)
 
-                except (requests.HTTPError, OSError) as e:
-                    st.write(f"Error loading image: {e}")
+            except (requests.HTTPError, OSError) as e:
+                st.write(f"Error loading image: {e}")
 
-            # Export CSV button
-            csv_data = [(book['title'], book['authors'].split(',')[0].strip()) for _, book in grouped_data.iterrows()]
-            csv_file = export_csv(csv_data)
-            st.markdown(f"### [Download Recommended Books CSV](data:file/csv;base64,{base64.b64encode(open(csv_file, 'rb').read()).decode()})")
+        # Export CSV button
+        csv_data = [(book['title'], book['authors'].split(',')[0].strip()) for _, book in grouped_data.iterrows()]
+        csv_file = export_csv(csv_data)
+        st.markdown(f"### [Download Recommended Books CSV](data:file/csv;base64,{base64.b64encode(open(csv_file, 'rb').read()).decode()})")
