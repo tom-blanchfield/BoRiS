f288fa45c7aa0172fe85997b256a096dd83c7527
tom-blanchfield <74678154+tom-blanchfield@users.noreply.github.com>
2025-02-26 18:45:51 +0000

    Update data.py

--- data.py
+++ data.py
@@ -6,7 +6,7 @@ import streamlit as st
 import altair as alt
 import seaborn as sns
 import matplotlib.pyplot as plt
-st.set_option('deprecation.showPyplotGlobalUse', False)
+# st.set_option('deprecation.showPyplotGlobalUse', False)
 
 # Load the data
 books = pd.read_csv('books.csv')
