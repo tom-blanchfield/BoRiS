1065c0e22d96644cc3535bf4524f0b7436dcc5cc
tom-blanchfield <74678154+tom-blanchfield@users.noreply.github.com>
2025-10-21 01:02:50 +0100

    Update data.py

--- data.py
+++ data.py
@@ -81,21 +81,21 @@ if option == "Ratings":
     #This chart will only display if expanded but it's worth it!
     
     # Add a chart to show the number of times raters have rated
-    #st.title ("This chart is hidden on load, click the expand button ->")
-    #chart_data = ratings['user_id'].value_counts().reset_index()
-    #chart_data.columns = ['user_id', 'rating_count']
-    #bars = alt.Chart(chart_data).mark_bar().encode(
-    #    x='rating_count',
-    #    y=alt.Y('user_id:O', sort='-x'),
-    #    tooltip=['user_id', 'rating_count']
-    #).properties(
-    #    title='Number of Times Raters Have Rated'
-    #)
-    #st.altair_chart(bars, use_container_width=True)
+    st.title ("This chart is hidden on load, click the expand button ->")
+    chart_data = ratings['user_id'].value_counts().reset_index()
+    chart_data.columns = ['user_id', 'rating_count']
+    bars = alt.Chart(chart_data).mark_bar().encode(
+        x='rating_count',
+        y=alt.Y('user_id:O', sort='-x'),
+        tooltip=['user_id', 'rating_count']
+    ).properties(
+        title='Number of Times Raters Have Rated'
+    )
+    st.altair_chart(bars, use_container_width=True)
     
     #Show the ratings dataframe
-    #st.header("Ratings Data")
-    #st.dataframe(ratings)  
+    st.header("Ratings Data")
+    st.dataframe(ratings)  
     
     # Add a button to generate cosine similarities for 100 top raters
 if option == "Get cosine similarities of top 100 raters":
